46b31200e93158f20a410781c2bb917be62eb943
boromirl <burdiyanov@gmail.com>
2025-09-23 17:34:33 +0700

    jcurses test

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,23 +1,11 @@
-import jcurses.system.*;
-import jcurses.widgets.*;
-import jcurses.util.*;
-import jcurses.event.*;
-import sun.misc.*;
-
 public class Main {
-    public static void main(String[] args) {
-        Toolkit.init();
-
-        final CharColor charColor = new CharColor(CharColor.WHITE, CharColor.RED);
-        Toolkit.printString("Hello world", 0, 0, charColor);
-
+    public static void main(String[] args){
+        String[] testArgs = {"Hello", "world"};
+        
         try {
-            Thread.sleep(10000);  // 3000 milliseconds = 3 seconds
-        } catch (InterruptedException e) {
-            // Handle interruption if needed
-            Thread.currentThread().interrupt(); // Restore interrupt flag
+            MainWindow.main(testArgs);
+        } catch (Exception e){
+            
         }
-
-        Toolkit.shutdown();
     }
 }
